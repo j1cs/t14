0122f0b5b39e5664ee109e5a607bee4e4bf1ac4f
Juan Cuzmar <jcuzmar@protonmail.com>
2021-08-31 22:46:08 -0400

    new config

--- .scripts/popup-calendar.sh
+++ .scripts/popup-calendar.sh
@@ -3,7 +3,7 @@
 BAR_HEIGHT=58 # polybar height
 BORDER_SIZE=0  # border size from your wm settings
 YAD_WIDTH=222
-YAD_HEIGHT=188
+YAD_HEIGHT=199
 DATE=" $(date +"%d-%m-%Y")   $(date +"%H:%M")"
 
 case "$1" in
